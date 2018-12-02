dbd4f697c47cab950014d81014199036db698f64
Josemi213 <jm.rojassanchez@ugto.mx>
2018-12-02 13:38:18 -0600

    Primer parte del proyecto.

---
+++ proyectofinal_main.c
@@ -0,0 +1,8 @@
+/*Proyecto final, José Misael Rojas Sánchez (Josemi213), Brandon Emmanuel Hernandez Corona (BrandonIB) */
+
+//Declaramos las librerias que utilizaremos.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
