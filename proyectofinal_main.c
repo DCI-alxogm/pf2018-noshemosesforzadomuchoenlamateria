977d0abdadcadba9a37ab6ddbfef72bfce96f3aa
BrandonIB <42146715+BrandonIB@users.noreply.github.com>
2018-12-03 09:25:48 -0600

    avance

--- proyectofinal_main.c
+++ proyectofinal_main.c
@@ -6,3 +6,24 @@
 #include<stdlib.h>
 #include<math.h>
 
+int main()
+{
+FILE*fp=NULL;
+char*nombrearchivo="entrada";
+ float entrada[24];
+ float*entradamem;
+ entradamem=malloc(24*sizeof(float));
+ int i=0;
+ fp = fopen(nombrearchivo, "r");
+ if(fp==NULL)return -1;
+ for (i=0;i<=24;i++)
+ {
+     fscanf(fp,"%f",&entrada[i]);
+     entradamem=&entrada[i];
+     //printf("%f\n", entrada[i]);
+     //printf("%p\n", entradamem);
+     printf("%f\n", *entradamem);
+ }
+
+return 0;
+}
