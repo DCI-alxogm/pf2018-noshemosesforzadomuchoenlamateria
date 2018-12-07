5ffdc6d044122ded5f9164560db0689f980474e6
Josemi213 <jm.rojassanchez@ugto.mx>
2018-12-06 21:55:06 -0600

    Avance de pointers

---
+++ proyecto_funciones.c
@@ -0,0 +1,15 @@
+/*Creado por Misael Rojas*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void print(double **aux, int w, int a);
+double **itera(double **aux, int w);
+double iniciar(double **aux, int w, float x, float y, float z)
+
+int main(){
+
+	double **mat;
+
+	float x, y, z, 
+}
