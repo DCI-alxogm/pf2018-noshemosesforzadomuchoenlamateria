8d93136d14f00eaad92839bed2c8a84ddcfce993
Josemi213 <jm.rojassanchez@ugto.mx>
2018-12-06 21:10:53 -0600

    Opciones

---
+++ proyectofinal_main1.c
@@ -0,0 +1,34 @@
+/*Proyecto final, José Misael Rojas Sánchez (Josemi213), Brandon Emmanuel Hernandez Corona (BrandonIB) */
+
+//Declaramos las librerias que utilizaremos.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
+
+double **iniciar(double **aux, int w, float x, float y, float z int filas){
+
+srand48(time(NULL));
+num_random = drand48()
+
+int matriz[filas][columnas]
+
+printf( "Introduzca el numero de filas: ");
+scanf("%i", &filas);
+
+x = drand48() % RAND_MAX
+y = drand48() % RAND_MAX
+z = drand48() % RAND_MAX
+
+
+void print(double **aux, int w, int a){
+
+	char name[15]={'C', 'o', 'r', 'd', 'e', 'n', 'a', 'd', 'a', 's', '_', 'A'};
+
+	name[11]= A + 'A';
+	char *coord;
+
+	coord=name
+	FILE *write;
+}
